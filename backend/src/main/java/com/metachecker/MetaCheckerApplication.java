12da012bef1dce5c42951e2383b544cdfb21a0c9
Кирилл <ustinovk722@gmail.com>
2025-10-29 00:16:01 +0300

    Реализован сбор метаданных с фото и ссылок

---
+++ backend/src/main/java/com/metachecker/MetaCheckerApplication.java
@@ -0,0 +1,11 @@
+package com.metachecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaCheckerApplication{
+    public static void main(String[] args){
+        SpringApplication.run(MetaCheckerApplication.class, args);
+    }
+} 
